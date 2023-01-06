98b4472932ef10ea8d7dcdbe498761fc8cd08d91
ravindrajain-hash <ravindra.jain@digimonk.in>
2023-01-06 15:05:55 +0530

    safari browser issue

--- app/Http/Controllers/Company/CustomerController.php
+++ app/Http/Controllers/Company/CustomerController.php
@@ -279,6 +279,9 @@ class CustomerController extends Controller
        } else {
         $wclass = "active-focus";
        }
+
+      $userData = User::select('openingtime','closingtime')->where('id',$auth_id)->first();
+
       $html ='<div class="add-customer-modal">
                 <h5>Create New Ticket</h5>
                </div>';
@@ -335,10 +338,49 @@ class CustomerController extends Controller
       </div>
       <div class="form-group col-md-6 mb-3">
         <label style="position: relative;left: 12px;margin-bottom: 11px;">Time</label>
-        <div class="input-group date" id="timePicker">
-          <input type="time" class="form-control time time-pickable timePicker" onkeypress="return onlyNumberKey(event)" onkeydown="return /[a-z]/i.test(event.key)" onpaste="return false" name="giventime" id="time" placeholder="Time" required><span class="input-group-addon" style="display:none;"><i class="fa fa-clock-o" aria-hidden="true"></i></span>
-        </div>
-      </div>
+        <select class="form-control selectpicker" aria-label="Default select example" data-placeholder="Select Time" data-live-search="true" name="giventime" id="time" style="height:auto;" required="">
+        <option value="08:00 am">08:00 am</option>
+        <option value="08:30 am">08:30 am</option>
+        <option value="09:00 am">09:00 am</option>
+        <option value="09:30 am">09:30 am</option>
+        <option value="10:00 am">10:00 am</option>
+        <option value="10:30 am">10:30 am</option>
+        <option value="11:00 am">11:00 am</option>
+        <option value="11:30 am">11:30 am</option>
+        <option value="12:00 pm">12:00 pm</option>
+        <option value="12:30 pm">12:30 pm</option>
+        <option value="01:00 pm">1:00 pm</option>
+        <option value="01:30 pm">01:30 pm</option>
+        <option value="02:00 pm">02:00 pm</option>
+        <option value="02:30 pm">02:30 pm</option>
+        <option value="03:00 pm">03:00 pm</option>
+        <option value="03:30 pm">03:30 pm</option>
+        <option value="04:00 pm">04:00 pm</option>
+        <option value="04:30 pm">04:30 pm</option>
+        <option value="05:00 pm">05:00 pm</option>
+        <option value="05:30 pm">05:30 pm</option>
+        <option value="06:00 pm">06:00 pm</option>
+        <option value="06:30 pm">06:30 pm</option>
+        <option value="07:00 pm">07:00 pm</option>
+        <option value="07:30 pm">07:30 pm</option>
+        
+        <option value="12:00 am">12:00 am</option>
+        <option value="12:30 am">12:30 am</option>
+        <option value="01:00 am">01:00 am</option>
+        <option value="01:30 am">01:30 am</option>
+        <option value="02:00 am">02:00 am</option>
+        <option value="02:30 am">02:30 am</option>
+        <option value="03:00 am">03:00 am</option>
+        <option value="03:30 am">03:30 am</option>
+        <option value="04:00 am">04:00 am</option>
+        <option value="04:30 am">04:30 am</option>
+        <option value="05:00 am">05:00 am</option>
+        <option value="05:30 am">05:30 am</option>
+        <option value="06:00 am">06:00 am</option>
+        <option value="06:30 am">06:30 am</option>
+        <option value="07:00 am">07:00 am</option>
+        <option value="07:30 am">07:30 am</option>';
+      $html .='</select></div>
       <div class="col-md-6 mb-3">
      <label style="position: relative;left: 12px;margin-bottom: 11px;">Date</label>
       <input type="date" class="form-control etc" placeholder="Date" name="date" id="date" onkeydown="return false" style="position: relative;" required>
